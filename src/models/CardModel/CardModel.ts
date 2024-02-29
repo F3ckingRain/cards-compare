ec76e9a2f318ed7546e9c4cf5ec04f2ee5a77a4e
VKryukov <kv-xlab@yandex.ru>
2024-02-29 17:20:30 +0500

    Logic closing cards if they dont compare

--- src/models/CardModel/CardModel.ts
+++ src/models/CardModel/CardModel.ts
@@ -1,8 +1,10 @@
 import useCardsState from "@/store/cardsState/cardsState";
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
 const CardModel = () => {
   const [state, setState] = useCardsState();
+
+  const openedCards = state.filter((el) => el.opened);
 
   const toggleCard = useCallback(
     (id: number) => {
@@ -17,6 +19,31 @@ const CardModel = () => {
     },
     [state],
   );
+
+  const compareTwoCards = useCallback(
+    (firstId: number, secondId: number) => {
+      const firstElem = state.find((el) => el.id === firstId)!;
+      const secondElem = state.find((el) => el.id === secondId)!;
+
+      return firstElem.image === secondElem.image;
+    },
+    [state],
+  );
+
+  const closeAllCards = useCallback(() => {
+    setState((s) => s.map((el) => ({ ...el, opened: false })));
+  }, []);
+
+  useEffect(() => {
+    if (openedCards.length != 2) return;
+
+    const cardsCompare = compareTwoCards(
+      openedCards.at(0)!.id,
+      openedCards.at(1)!.id,
+    );
+
+    if (!cardsCompare) setTimeout(() => closeAllCards(), 1000);
+  }, [openedCards, closeAllCards, compareTwoCards]);
 
   const model = useMemo(
     () => ({
--- src/store/cardsState/index.ts
+++ src/store/cardsState/index.ts
@@ -1,4 +1,5 @@
 import card from "@/assets/cards/1200px-French_suits.svg.png";
+import otherCard from "@/assets/cards/Atlas_deck_3_of_hearts.svg.png";
 
 type cardItemType = {
   id: number;
@@ -8,6 +9,10 @@ type cardItemType = {
 
 const initialCardsState: cardItemType[] = [
   { image: card, opened: false, id: 1 },
+  { image: otherCard, opened: false, id: 2 },
+  { image: card, opened: false, id: 3 },
+  { image: otherCard, opened: false, id: 4 },
+  { image: card, opened: false, id: 5 },
 ];
 
 export { cardItemType, initialCardsState };
